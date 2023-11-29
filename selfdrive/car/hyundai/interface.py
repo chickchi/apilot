df90733cd8866d0ca4f6d297b239273f63665e70
chickchi <87925125+chickchi@users.noreply.github.com>
2023-11-30 06:01:48 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -316,7 +316,7 @@ class CarInterface(CarInterfaceBase):
     ret.stoppingControl = True
     ret.startingState = False  # False # startAccel을 적용하는 startingState를 막음.
     ret.vEgoStarting = 0.3
-    ret.vEgoStopping = 0.5
+    ret.vEgoStopping = 0.4
     ret.startAccel = 2.0
     ret.stoppingDecelRate = 1.2 # brake_travel/s while trying to stop
     ret.longitudinalActuatorDelayLowerBound = 0.5
