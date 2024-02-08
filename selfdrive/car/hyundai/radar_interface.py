6b67e7203ccfc59bc80789091002a9d1c1bd412b
chickchi <87925125+chickchi@users.noreply.github.com>
2024-02-08 22:03:30 +0900

    Update radar_interface.py

--- selfdrive/car/hyundai/radar_interface.py
+++ selfdrive/car/hyundai/radar_interface.py
@@ -29,6 +29,7 @@ def get_radar_can_parser(CP):
         ("REL_SPEED", msg),
       ]
       checks += [(msg, 20)]
+      #checks += [(msg, 50)]
     print("RadarInterface: RadarTracks..")
     #return CANParser(DBC[CP.carFingerprint]['radar'], signals, checks, 1)
     return CANParser('hyundai_kia_mando_front_radar_generated', signals, checks, 1)
@@ -44,6 +45,7 @@ def get_radar_can_parser(CP):
     ]
     checks = [
       ("SCC11", 20),
+      #("SCC11", 50),
     ]
     print("RadarInterface: SCCRadar...")
     return CANParser(DBC[CP.carFingerprint]['pt'], signals, checks, CP.sccBus)
