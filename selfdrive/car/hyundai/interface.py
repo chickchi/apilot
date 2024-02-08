69f43ba2da80350a42f331e8d96752a8cf959e13
chickchi <87925125+chickchi@users.noreply.github.com>
2024-02-08 22:02:03 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -316,14 +316,14 @@ class CarInterface(CarInterfaceBase):
     ret.stoppingControl = True
     ret.startingState = True  # False # startAccel을 적용하는 startingState를 막음.
     ret.vEgoStarting = 0.2
-    ret.vEgoStopping = 0.3
+    ret.vEgoStopping = 0.2
     ret.startAccel = 1.0 #2.0
     ret.stoppingDecelRate = 1.0 # brake_travel/s while trying to stop
     ret.longitudinalActuatorDelayLowerBound = 0.5
     ret.longitudinalActuatorDelayUpperBound = 0.5
 
-    #ret.radarTimeStep = (1.0 / 50) # 50Hz   SCC11, RadarTrack은 50Hz
-    ret.radarTimeStep = (1.0 / 20) # 20Hz 레이더부하줄일겸..
+    ret.radarTimeStep = (1.0 / 50) # 50Hz   SCC11, RadarTrack은 50Hz
+    #ret.radarTimeStep = (1.0 / 20) # 20Hz 레이더부하줄일겸..
 
     # *** feature detection ***
     if candidate in CANFD_CAR:
