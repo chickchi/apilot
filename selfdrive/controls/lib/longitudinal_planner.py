5caa4bc9c43a05b4d41aa55cbddaa0294611fb27
chickchi <87925125+chickchi@users.noreply.github.com>
2026-04-18 13:10:59 +0900

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -18,7 +18,7 @@ from common.params import Params
 LON_MPC_STEP = 0.2  # first step is 0.2s
 A_CRUISE_MIN = -1.2
 
-A_CRUISE_MAX_VALS = [1.2, 1.1, 1.0, 0.85, 0.70]
+A_CRUISE_MAX_VALS = [1.2, 1.1, 1.0, 0.85, 0.75]
 A_CRUISE_MAX_BP = [0., 20*CV.KPH_TO_MS, 40*CV.KPH_TO_MS, 60*CV.KPH_TO_MS, 80*CV.KPH_TO_MS]
 
 # Lookup table for turns
